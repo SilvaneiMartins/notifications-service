634937b3d3008d394b5ffaa5c536a0b76ae916cc
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-12-14 10:49:33 -0300

    Criando use-cases send-notification

---
+++ src/app/use-cases/send-notifications.ts
@@ -0,0 +1,30 @@
+import { Content } from '../entities/content';
+import { Notification } from '../entities/notification';
+
+interface ISendNotificationRequest {
+    recipientId: string;
+    content: string;
+    category: string;
+}
+
+interface ISendNotificationResponse {
+    notification: Notification;
+}
+
+export class SendNotification {
+    async execute(
+        request: ISendNotificationRequest,
+    ): Promise<ISendNotificationResponse> {
+        const { recipientId, content, category } = request;
+
+        const notification = new Notification({
+            recipientId,
+            content: new Content(content),
+            category,
+        });
+
+        return {
+            notification,
+        };
+    }
+}
