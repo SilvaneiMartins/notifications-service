9cac92378cbf435a87365477411674fc74acd025
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-12-14 11:21:05 -0300

    Atualizando e realizando teste na aplicação

--- src/app/use-cases/send-notifications.spec.ts
+++ src/app/use-cases/send-notifications.spec.ts
@@ -1,26 +1,18 @@
 import { SendNotification } from './send-notifications';
-import { Notification } from '../entities/notification';
-
-const notificatios: Notification[] = [];
-
-const notificationsRepository = {
-    async create(notification: Notification) {
-        notificatios.push(notification);
-    },
-};
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
 
 describe('Send Notification', () => {
     it('should be able to send a notifications', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository();
         const sendNotification = new SendNotification(notificationsRepository);
 
-        await sendNotification.execute({
+        const { notification } = await sendNotification.execute({
             content: 'This is a notification',
             category: 'notification',
             recipientId: 'example-recipient-id',
         });
 
-        console.log(notificatios);
-
-        expect(notificatios).toHaveLength(1);
+        expect(notificationsRepository.notificatios).toHaveLength(1);
+        expect(notificationsRepository.notificatios[0]).toEqual(notification);
     });
 });
